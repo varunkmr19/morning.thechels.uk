96ae844b22609a4135a47a1417e95449c7be579c
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-15 13:51:17 +0100

    updated
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/processing.py
+++ python/processing.py
@@ -13,27 +13,27 @@ import feedparser
 # setup
 root = pathlib.Path(__file__).parent.parent.resolve()
 url_list = [
-    "http://daringfireball.net/index.xml",
-    # "http://feeds.feedburner.com/macstoriesnet",
+      "http://daringfireball.net/index.xml",
+      "http://feeds.feedburner.com/macstoriesnet",
     # "http://thechels.uk/feed/",
     # "http://xkcd.com/rss.xml",
       "https://www.cloudflarestatus.com/history.rss",
-    # "http://blog.agilebits.com/feed/",
+      "http://blog.agilebits.com/feed/",
       "https://status.dropbox.com/history.rss",
       "http://feeds.feedburner.com/Garmin",
-    # "http://status.ifttt.com/history.rss",
-    # "http://blog.mailgun.net/rss",
-    # "http://blog.strava.com/feed/atom/",
+      "http://status.ifttt.com/history.rss",
+      "http://blog.mailgun.net/rss",
+      "http://blog.strava.com/feed/atom/",
       "http://blog.supertop.co/rss",
-    # "https://blog.dropbox.com/feed/",
+      "https://blog.dropbox.com/feed/",
       "https://medium.com/feed/strava-engineering",
       "http://www.politics.co.uk/rss.xml",
-    # "https://sixcolors.com/feed.json",
-    # "http://feeds.feedburner.com/ReflectivePerspective",
-    # "http://the5krunner.com/feed/",
-    # "https://www.troyhunt.com/rss/",
-    # "http://usesthis.com/feed/",
-     "https://wonkhe.com/blogs/feed/"
+      "https://sixcolors.com/feed.json",
+      "http://feeds.feedburner.com/ReflectivePerspective",
+      "http://the5krunner.com/feed/",
+      "https://www.troyhunt.com/rss/",
+      "http://usesthis.com/feed/",
+      "https://wonkhe.com/blogs/feed/"
 ]
 
 # Replacer function
@@ -56,6 +56,11 @@ def fetch_blog_entries(working_url):
         }
         for entry in entries
     ]
+# get domain Function
+def get_domain_name_from_url(url):
+    t = urlparse(url).netloc
+    return ('.'.join(t.split('.')[1:]))
+
 
 # processing
 if __name__ == "__main__":
@@ -64,7 +69,8 @@ if __name__ == "__main__":
     index_contents = index_page.open().read()
     for url in url_list:
         entries = fetch_blog_entries(url)[:1]
-        data_item_text = "\n\n".join(["<p>{title}</p><br/><small><a href='{url}'>Published: {published}</a></small>".format(**entry) for entry in entries])
+        domain = get_domain_name_from_url(url)
+        data_item_text = "\n\n".join(["<p><a href='{url}'>{title}</a><br/><small>{domain} | Published: {published}</small></p>".format(**entry) for entry in entries])
         all_news += data_item_text
     final_output = replace_chunk(index_contents, "content_marker", all_news)
     index_page.open("w").write(final_output)
