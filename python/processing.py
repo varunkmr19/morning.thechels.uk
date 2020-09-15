d56b1d0dc6b073ec4972faed70a855fc12f86d46
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-15 08:49:03 +0100

    once more with feeling
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/processing.py
+++ python/processing.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     index_contents = index_page.open().read()
     for url in url_list:
         entries = fetch_blog_entries(url)[:1]
-        data_item_text = "\n".join(["<p>{title}</p><p><small><a href='{url}'>Published: {published}</a></small></p>\n".format(**entry) for entry in entries])
+        data_item_text = "\n\n".join(["<p>{title}</p><p><small><a href='{url}'>Published: {published}</a></small></p>".format(**entry) for entry in entries])
         all_news += data_item_text
     final_output = replace_chunk(index_contents, "content_marker", all_news)
     index_page.open("w").write(final_output)
