422c24649fbbb0844b866620b8053e60d26a007d
varun <varun-kumar@live.com>
2020-09-25 07:52:23 +0530

    fixed format for expected rain percentage

--- python/weather.py
+++ python/weather.py
@@ -9,7 +9,8 @@ from datetime import date
 root = pathlib.Path(__file__).parent.parent.resolve()
 location = os.getenv('city_code')+','+os.getenv('country_code')
 APIKEY = os.getenv('open_weather_key')
-url = "http://api.openweathermap.org/data/2.5/find?q=%s&units=metric&APPID=%s" %(location,APIKEY)
+url = "http://api.openweathermap.org/data/2.5/find?q=%s&units=metric&APPID=%s" % (
+    location, APIKEY)
 # headers = {"Authorization":"Bearer %s"%key}
 
 response = requests.get(url)
@@ -19,24 +20,28 @@ my_loc = response_dict['list'][0]
 today_weather = str(my_loc['main']['temp'])
 high_temp = str(my_loc['main']['temp_max'])
 low_temp = str(my_loc['main']['temp_min'])
-rain = str(my_loc['rain'])
+rain = str(my_loc['rain']['1h'])
 today_desc = str(my_loc['weather'][0]['description'])
 
 if(rain == "None"):
     rain = "0"
 
-string_today =  f"Today's date is {output_date}, Here is your daily briefing..."
+string_today = f"Today's date is {output_date}, Here is your daily briefing..."
 string_today += f"The average temperature today is {today_weather}˚C with highs of {high_temp}˚C and lows of {low_temp}˚C. "
 string_today += f"You should expect {today_desc} with a {rain}% chance of rain."
 
 # Replacer function
+
+
 def replace_chunk(content, marker, chunk):
     replacer = re.compile(
         r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
         re.DOTALL,
     )
-    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(
+        marker, chunk, marker)
     return replacer.sub(chunk, content)
+
 
 # processing
 if __name__ == "__main__":
